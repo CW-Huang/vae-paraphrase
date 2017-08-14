c41959bac9e09a49ec10821564282891a63c895e
Shawn Tan <shawn@wtf.sg>
2017-08-14 16:15:13 -0400

    Changes to LSTM - no subtensor required.

--- lstm.py
+++ lstm.py
@@ -15,12 +15,8 @@ def weight_init(input_size, output_size, factor=1):
     )
 
 
-def transition_init(size, fan_out):
-    W = np.empty((size, fan_out * size), dtype=np.float32)
-    for i in xrange(fan_out):
-        W[:, i * size:(i + 1) * size] = \
-            scipy.linalg.orth(np.random.randn(size, size))
-    return W
+def transition_init(size):
+    return scipy.linalg.orth(np.random.randn(size, size))
 
 
 def build(P, name, input_size, hidden_size, truncate_gradient=-1):
@@ -47,84 +43,79 @@ def build(P, name, input_size, hidden_size, truncate_gradient=-1):
 
 
 def build_step(P, name, input_sizes, hidden_size):
-    name_W_hidden = "W_%s_hidden" % name
-    name_W_cell = "W_%s_cell" % name
-    name_b = "b_%s" % name
+    b = {}
+    W = {}
+    V = {}
+    for gate in ('i', 'f', 'c', 'o'):
 
-    W_inputs = []
-    W = weight_init(sum(input_sizes), hidden_size * 4)
-    acc = 0
-    for i, input_size in enumerate(input_sizes):
-        name_W = "W_%s_input_%d" % (name, i)
-        P[name_W] = W[acc:acc + input_size]
-        W_inputs.append(P[name_W])
-        acc += input_size
+        pname = "b_%s_%s" % (name, gate)
+        P[pname] = np.zeros((hidden_size,))
+        b[gate] = P[pname]
 
-    P[name_W_hidden] = transition_init(hidden_size, 4)
-    P[name_W_cell] = transition_init(hidden_size, 3)
-    bias_init = np.zeros((4, hidden_size), dtype=np.float32)
-    bias_init[1] = 0
-    P[name_b] = bias_init
+        for i, input_size in enumerate(input_sizes):
+            pname = "W_%s_input_%d_%s" % (name, i, gate)
+            P[pname] = weight_init(input_size, hidden_size)
+            W[i, gate] = P[pname]
 
-    V_if = P[name_W_cell][:, 0 * hidden_size:2 * hidden_size]
-    V_o = P[name_W_cell][:, 2 * hidden_size:3 * hidden_size]
-
-    biases = P[name_b]
-    b_i = biases[0]
-    b_f = biases[1]
-    b_c = biases[2]
-    b_o = biases[3]
+        for prev in ('h', 'c'):
+            if gate == 'c' and prev == 'c':
+                continue
+            else:
+                pname = "W_%s_%s_%s" % (name, prev, gate)
+                P[pname] = transition_init(hidden_size)
+                V[prev, gate] = P[pname]
 
     def _step(*args):
         inputs = args[:len(input_sizes)]
-        [prev_cell, prev_hid] = args[len(input_sizes):]
+        [prev_cell, prev_hidden] = args[len(input_sizes):]
 
-        # batch_size x hidden_size
-        batch_size = inputs[0].shape[0]
+        i_gate = T.nnet.sigmoid(
+            sum(T.dot(x, W[i, 'i']) for i, x in enumerate(inputs)) +
+            T.dot(prev_hidden, V['h', 'i']) +
+            T.dot(prev_cell, V['c', 'i']) +
+            b['i']
+        )
 
-        # batch_size x 4 x hidden_size
-        transformed_x = sum(
-                T.dot(x, W).reshape((batch_size, 4, hidden_size))
-                for x, W in zip(inputs, W_inputs)
-            )
+        f_gate = T.nnet.sigmoid(
+            sum(T.dot(x, W[i, 'f']) for i, x in enumerate(inputs)) +
+            T.dot(prev_hidden, V['h', 'f']) +
+            T.dot(prev_cell, V['c', 'f']) +
+            b['f']
+        )
 
-        # batch_size x 4 x hidden_size
-        transformed_hid = T.dot(prev_hid, P[name_W_hidden]).reshape(
-            (batch_size, 4, hidden_size))
-        # batch_size x 2 x hidden_size
-        transformed_cell = T.dot(prev_cell, V_if).reshape(
-            (batch_size, 2, hidden_size))
+        cell_ = T.tanh(
+            sum(T.dot(x, W[i, 'c']) for i, x in enumerate(inputs)) +
+            T.dot(prev_hidden, V['h', 'c']) +
+            b['c']
+        )
+        cell = f_gate * prev_cell + i_gate * cell_
 
-        x_i = transformed_x[:, 0]
-        x_f = transformed_x[:, 1]
-        x_c = transformed_x[:, 2]
-        x_o = transformed_x[:, 3]   # batch_size x hidden_size
+        o_gate = T.nnet.sigmoid(
+            sum(T.dot(x, W[i, 'o']) for i, x in enumerate(inputs)) +
+            T.dot(prev_hidden, V['h', 'o']) +
+            T.dot(cell, V['c', 'o']) +
+            b['o']
+        )
 
-        h_i = transformed_hid[:, 0]
-        h_f = transformed_hid[:, 1]
-        h_c = transformed_hid[:, 2]
-        h_o = transformed_hid[:, 3]  # batch_size x hidden_size
-
-        c_i = transformed_cell[:, 0]
-        c_f = transformed_cell[:, 1]  # batch_size x hidden_size
-
-        in_lin = x_i + h_i + b_i + c_i
-        forget_lin = x_f + h_f + b_f + c_f
-        cell_lin = x_c + h_c + b_c
-
-        in_gate = T.nnet.sigmoid(in_lin)
-        forget_gate = T.nnet.sigmoid(forget_lin)
-        cell_updates = T.tanh(cell_lin)
-        in_gate.name = "in_gate"
-        forget_gate.name = "forget_gate"
-        cell_updates.name = "cell_updates"
-
-        cell = forget_gate * prev_cell + in_gate * cell_updates
-
-        out_lin = x_o + h_o + b_o + T.dot(cell, V_o)
-        out_gate = T.nnet.sigmoid(out_lin)
-        out_gate.name = "out_gate"
-
-        hid = out_gate * T.tanh(cell)
-        return cell, hid
+        hidden = o_gate * T.tanh(cell)
+        return cell, hidden
     return _step
+
+if __name__ == "__main__":
+    from theano_toolkit.parameters import Parameters
+    from pprint import pprint
+    P = Parameters()
+    step_1 = build_step(
+        P, name="test_1",
+        input_sizes=[10],
+        hidden_size=20
+    )
+    pprint(P.values())
+    output = step_1(
+        np.random.randn(1, 10),
+        np.random.randn(1, 20),
+        np.random.randn(1, 20)
+    )
+    T.grad(T.sum(output), wrt=P.values())
+
+
