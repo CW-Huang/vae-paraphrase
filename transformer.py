66b7614f6047431cb19a011232cf294a2d0b43f5
Shawn Tan <shawn@wtf.sg>
2017-08-24 23:45:36 -0400

    Transformer layer

--- transformer.py
+++ transformer.py
@@ -13,50 +13,66 @@ def softmax(x, mask, axis=-1):
     assert(x.ndim == output.ndim)
     return output
 
-def build_self_attention(P, name, input_size, key_size):
-    P['W_%s_query' % name] = np.random.randn(input_size, key_size)
-    P['W_%s_key' % name] = np.random.randn(input_size, key_size)
-    P['w_%s_after' % name] = np.float32(0)
-    P['w_%s_before' % name] = np.float32(0)
-    P['b_%s' % name] = np.float32(0)
+def build_self_attention(P, name, input_size, key_size, heads=1):
+    P['W_%s_query' % name] = np.random.randn(heads, input_size, key_size)
+    P['W_%s_key' % name] = np.random.randn(heads, input_size, key_size)
+    P['w_%s_after' % name] = np.zeros((heads,))
+    P['w_%s_before' % name] = np.zeros((heads,))
+    P['b_%s' % name] = np.zeros((heads,))
+
     W_query = P['W_%s_query' % name]
     W_key = P['W_%s_key' % name]
     w_after = P['w_%s_after' % name]
     w_before = P['w_%s_before' % name]
     b = P['b_%s' % name]
+
     def self_attend(values, mask=None):
+        batch_size = values.shape[0]
+        length = values.shape[1]
+        # values : batch_size, length, input_size
         if mask is None:
             mask = T.ones_like(values[:, :, 0])
         mask = mask[:, :, None]
-        query = T.dot(values, W_query)
-        keys = T.dot(values, W_key)
-        time = T.arange(values.shape[1])
+        query = T.tensordot(values, W_query, axes=(2, 1))
+        keys = T.tensordot(values, W_key, axes=(2, 1))
+        # query & keys : batch_size, length, heads, key_size
+
+        query = query.dimshuffle(0, 2, 1, 3).reshape((
+            batch_size * heads, length, key_size
+        ))
+        keys = keys.dimshuffle(0, 2, 1, 3).reshape((
+            batch_size * heads, length, key_size
+        ))
+        # query & keys: batch_size * heads, length, key_size
+
+        time = T.arange(length)
         td = time[None, :] - time[:, None]
         current = T.neq(td, 0)
-        after = w_after * T.switch(td > 0, td, 0)
-        before = w_before * T.switch(td < 0, -td, 0)
-        # query : batch_size x length x key_size
-        # keys : batch_size x length x key_size
-        # values : batch_size x length x value_size
+        after = w_after[:, None, None] * T.switch(td > 0, td, 0)[None, :, :]
+        before = w_before[:, None, None] * T.switch(td < 0, -td, 0)[None, :, :]
+
+        outer_dot = T.batched_tensordot(
+            query, keys, axes=(2, 2)).reshape((
+                batch_size, heads, length, length
+            ))
+        # outer_dot: batch_size, heads, length, length
         attn = softmax(
-            (T.batched_tensordot(
-                 query, keys,
-                 axes=(2, 2)
-             ) / np.sqrt(key_size) +
-             after + before + b),
+            (outer_dot / np.sqrt(key_size)) +
+            after +
+            before +
+            b[None, :, None, None],
             mask and current,
             axis=-1
         )
-        from theano_toolkit import hinton
-        hinton.plot(attn.eval())
+        # attn : batch_size, heads, length, length
 
-        # attn : batch_size x length x length
-        output = T.batched_dot(attn, values)
+        output = T.batched_dot(attn, values).dimshuffle(0, 2, 1, 3)
+        # output : batch_size, length, heads, input_size
         return output
     return self_attend
 
 def build_transform_layer(P, name, input_size, output_size,
-                          key_size, hidden_size):
+                          key_size, hidden_size, heads=1):
     self_attend = build_self_attention(
         P, name="%s_attn" % name,
         input_size=input_size,
@@ -65,7 +81,7 @@ def build_transform_layer(P, name, input_size, output_size,
 
     layer_transform = feedforward.build_classifier(
         P, name="%s_transform" % name,
-        input_sizes=[input_size, input_size],
+        input_sizes=[input_size, input_size * heads],
         hidden_sizes=[hidden_size],
         output_size=output_size,
         initial_weights=feedforward.relu_init,
@@ -76,6 +92,11 @@ def build_transform_layer(P, name, input_size, output_size,
 
     def transform(X):
         selected = self_attend(X)
+        selected = selected.reshape((
+            selected.shape[0],
+            selected.shape[1], -1
+        ))
+
         _, output = layer_transform([X, selected])
         return output
     return transform
@@ -86,13 +107,13 @@ if __name__ == "__main__":
     P = Parameters()
     transform = build_transform_layer(
         P, name="test",
-        input_size=20,
-        output_size=30,
-        key_size=20,
-        hidden_size=20
+        input_size=10,
+        key_size=11,
+        hidden_size=12,
+        output_size=13,
     )
     X = T.as_tensor_variable(
-        np.random.randn(1, 10, 20)
+        np.random.randn(2, 20, 10)
     )
     print P.values()
     print transform(X).eval().shape
